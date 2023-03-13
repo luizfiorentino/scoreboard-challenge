60b0069a4954182426aff18344d37a7f80338c45
luizfiorentino <lgfiorentino@gmail.com>
2023-03-13 13:20:14 +0100

    feat- App.js returns one Scoreboard component for each player that can be added via the input field

--- src/App.js
+++ src/App.js
@@ -3,11 +3,34 @@ import { useState } from "react";
 import Scoreboard from "./scoreboard/Scoreboard";
 
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [newPlayer, setNewPlayer] = useState("");
+
+  const newGuy = (e) => {
+    setNewPlayer(e.target.value);
+  };
+
+  const addMe = () => {
+    const player = newPlayer;
+    setPlayers([...players, player]);
+    setNewPlayer("");
+    return players;
+  };
+  console.log("addPlayer", newPlayer, players);
   return (
     <div>
       <h1>Score board</h1>
-      <Scoreboard name="Hermes" />
-      <Scoreboard name="Renato" />
+      <p>New Player</p>
+      <input
+        type="text"
+        placeholder="name"
+        value={newPlayer}
+        onChange={(e) => setNewPlayer(e.target.value)}
+      />
+      <button onClick={addMe}>Add</button>
+      {players.map((player) => (
+        <Scoreboard name={player} />
+      ))}
     </div>
   );
 }
