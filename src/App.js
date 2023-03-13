c6acb4e0fbd2f1389326aae5b3faf56817417a62
luizfiorentino <lgfiorentino@gmail.com>
2023-03-13 11:57:40 +0100

    feat- Scoreboard is now a reusable component receiving props (name) from App.js

--- src/App.js
+++ src/App.js
@@ -1,101 +1,13 @@
 import "./App.css";
 import { useState } from "react";
+import Scoreboard from "./scoreboard/Scoreboard";
 
 function App() {
-  const [score, setScore] = useState(0);
-
-  let message;
-
-  if (score < 10) {
-    message = "A journey of a 1000 miles start with a single step";
-  } else if (score >= 10 && score < 45) {
-    message = "C'mon you can do it";
-  } else if (score >= 45 && score < 80) {
-    message = "You're getting there";
-  } else if (score >= 80) {
-    message = "Hold on! Finish strong";
-  }
-
-  const setMessage = () => {
-    setText(message);
-  };
-
-  const [text, setText] = useState(message);
-  //console.log("score, message:", score, message);
-  const displayMessage = message;
-
   return (
-    <div className="App">
-      <h1>Score Board</h1>
-      <h3>bar comes here ;)</h3>
-      <p>{score}/100</p>{" "}
-      {score >= 10 ? (
-        <button
-          onClick={() => {
-            setScore(score - score / 2);
-            setMessage(message);
-          }}
-        >
-          - 50%
-        </button>
-      ) : undefined}
-      {score >= 10 ? (
-        <button
-          onClick={() => {
-            setScore(score - 5);
-            setMessage(message);
-          }}
-        >
-          - 5
-        </button>
-      ) : undefined}{" "}
-      {score >= 10 ? (
-        <button
-          onClick={() => {
-            setScore(score - 1);
-            setMessage(message);
-          }}
-        >
-          - 1
-        </button>
-      ) : undefined}{" "}
-      {score > 0 && score >= 10 ? (
-        <button
-          onClick={() => {
-            setScore(0);
-            setMessage(message);
-          }}
-        >
-          Reset
-        </button>
-      ) : undefined}{" "}
-      <button
-        onClick={() => {
-          setScore(score + 1);
-          setMessage(message);
-        }}
-      >
-        + 1
-      </button>{" "}
-      <button
-        onClick={() => {
-          setScore(score + 5);
-          setMessage(message);
-        }}
-      >
-        + 5
-      </button>{" "}
-      {score >= 10 && score < 80 ? (
-        <button
-          onClick={() => {
-            setScore(score + score / 2);
-            setMessage(message);
-          }}
-        >
-          + 50%
-        </button>
-      ) : undefined}{" "}
-      <p>{displayMessage}</p>
+    <div>
+      <h1>Score board</h1>
+      <Scoreboard name="Hermes" />
+      <Scoreboard name="Renato" />
     </div>
   );
 }
---
+++ src/scoreboard/Scoreboard.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Scoreboard(props) {
+  const [score, setScore] = useState(0);
+
+  let message;
+
+  if (score < 10) {
+    message = "A journey of a 1000 miles start with a single step";
+  } else if (score >= 10 && score < 45) {
+    message = "C'mon you can do it";
+  } else if (score >= 45 && score < 80) {
+    message = "You're getting there";
+  } else if (score >= 80) {
+    message = "Hold on! Finish strong";
+  }
+
+  const setMessage = () => {
+    setText(message);
+  };
+
+  const [text, setText] = useState(message);
+  //console.log("score, message:", score, message);
+  const displayMessage = message;
+
+  return (
+    <div className="App">
+      <h1>{props.name}</h1>
+      <h3>bar comes here ;)</h3>
+      <p>{score}/100</p>{" "}
+      {score >= 10 ? (
+        <button
+          onClick={() => {
+            setScore(score - score / 2);
+            setMessage(message);
+          }}
+        >
+          - 50%
+        </button>
+      ) : undefined}
+      {score >= 10 ? (
+        <button
+          onClick={() => {
+            setScore(score - 5);
+            setMessage(message);
+          }}
+        >
+          - 5
+        </button>
+      ) : undefined}{" "}
+      {score >= 10 ? (
+        <button
+          onClick={() => {
+            setScore(score - 1);
+            setMessage(message);
+          }}
+        >
+          - 1
+        </button>
+      ) : undefined}{" "}
+      {score > 0 && score >= 10 ? (
+        <button
+          onClick={() => {
+            setScore(0);
+            setMessage(message);
+          }}
+        >
+          Reset
+        </button>
+      ) : undefined}{" "}
+      <button
+        onClick={() => {
+          setScore(score + 1);
+          setMessage(message);
+        }}
+      >
+        + 1
+      </button>{" "}
+      <button
+        onClick={() => {
+          setScore(score + 5);
+          setMessage(message);
+        }}
+      >
+        + 5
+      </button>{" "}
+      {score >= 10 && score < 80 ? (
+        <button
+          onClick={() => {
+            setScore(score + score / 2);
+            setMessage(message);
+          }}
+        >
+          + 50%
+        </button>
+      ) : undefined}{" "}
+      <p>{displayMessage}</p>
+    </div>
+  );
+}
