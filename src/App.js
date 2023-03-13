327fbbeae5f4e9c62c268d7af4af3c81e2ec05c4
luizfiorentino <lgfiorentino@gmail.com>
2023-03-13 10:38:01 +0100

    feat- displaying the message that corresponds to the current score

--- src/App.js
+++ src/App.js
@@ -3,18 +3,49 @@ import { useState } from "react";
 
 function App() {
   const [score, setScore] = useState(0);
-  const [text, setText] = useState(
-    "A journey of a 1000 miles start with a single step"
-  );
+
+  let message;
+
+  if (score < 10) {
+    message = "A journey of a 1000 miles start with a single step";
+  } else if (score >= 10 && score < 45) {
+    message = "C'mon you can do it";
+  } else if (score >= 45 && score < 80) {
+    message = "You're getting there";
+  } else if (score >= 80) {
+    message = "Hold on! Finish strong";
+  }
+
+  const setMessage = () => {
+    setText(message);
+  };
+
+  const [text, setText] = useState(message);
+  //console.log("score, message:", score, message);
+  const displayMessage = message;
 
   return (
     <div className="App">
       <h1>Score Board</h1>
       <h3>bar comes here ;)</h3>
-      <p>{score}/100</p>
-      <button onClick={() => setScore(score + 1)}>+</button>{" "}
-      <button onClick={() => setScore(score - 1)}>-</button>
-      <p>{text}</p>
+      <p>{score}/100</p>{" "}
+      <button
+        onClick={() => {
+          setScore(score - 1);
+          setMessage(message);
+        }}
+      >
+        -
+      </button>{" "}
+      <button
+        onClick={() => {
+          setScore(score + 1);
+          setMessage(message);
+        }}
+      >
+        +
+      </button>{" "}
+      <p>{displayMessage}</p>
     </div>
   );
 }
