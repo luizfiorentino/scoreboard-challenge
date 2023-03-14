fcb17ec1b3f16f42c78fdb5edb2a7c640fa5f950
luizfiorentino <lgfiorentino@gmail.com>
2023-03-14 13:54:57 +0100

    feat- score bar added reflecting the player's score

--- src/scoreboard/Scoreboard.jsx
+++ src/scoreboard/Scoreboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import "./styles.css";
 
 export default function Scoreboard(props) {
   const [score, setScore] = useState(0);
@@ -25,85 +26,100 @@ export default function Scoreboard(props) {
   //console.log("score, message:", score, message);
   const displayMessage = message;
 
+  const width = (props.score / 100) * 290;
+
   return (
     <div className="App">
       <h3>{props.name}</h3>
-      <h3>bar comes here ;)</h3>
-      <p>{props.score}/100</p>{" "}
-      {props.score >= 10 ? (
+      <div className="container">
+        <h3 className="title">Score</h3>
+        <div className="bar">
+          <div
+            className="inner-bar"
+            data-percentage="70%"
+            style={{ width: width }}
+          ></div>
+          <script src="main.js"></script>
+        </div>
+      </div>
+      {/* <h3>bar comes here ;)</h3> */}
+      <div className="body">
+        <p>{props.score}/100</p>{" "}
+        {props.score >= 10 ? (
+          <button
+            // onClick={() => {
+            //   setScore(score - score / 2);
+            //   setMessage(message);
+            // }}
+            onClick={() => props.decrementFifty(props.id)}
+          >
+            - 50%
+          </button>
+        ) : undefined}
+        {props.score >= 10 ? (
+          <button
+            // onClick={() => {
+            //   setScore(score - 5);
+            //   setMessage(message);
+            // }}
+            onClick={() => props.decrementByFive(props.id)}
+          >
+            - 5
+          </button>
+        ) : undefined}{" "}
+        {props.score >= 10 ? (
+          <button
+            // onClick={() => {
+            //   setScore(score - 1);
+            //   setMessage(message);
+            // }}
+            onClick={() => props.decrementScoreByOne(props.id)}
+          >
+            - 1
+          </button>
+        ) : undefined}{" "}
+        {props.score > 0 && props.score >= 10 ? (
+          <button
+            // onClick={() => {
+            //   setScore(0);
+            //   setMessage(message);
+            // }}
+            onClick={() => props.resetScore(props.id)}
+          >
+            Reset
+          </button>
+        ) : undefined}{" "}
         <button
           // onClick={() => {
-          //   setScore(score - score / 2);
+          //   setScore(score + 1);
           //   setMessage(message);
           // }}
-          onClick={() => props.decrementFifty(props.id)}
+          onClick={() => props.incrementScoreByOne(props.id)}
         >
-          - 50%
-        </button>
-      ) : undefined}
-      {props.score >= 10 ? (
+          + 1
+        </button>{" "}
         <button
           // onClick={() => {
-          //   setScore(score - 5);
+          //   setScore(score + 5);
           //   setMessage(message);
           // }}
-          onClick={() => props.decrementByFive(props.id)}
+          onClick={() => props.incrementByFive(props.id)}
         >
-          - 5
-        </button>
-      ) : undefined}{" "}
-      {props.score >= 10 ? (
-        <button
-          // onClick={() => {
-          //   setScore(score - 1);
-          //   setMessage(message);
-          // }}
-          onClick={() => props.decrementScoreByOne(props.id)}
-        >
-          - 1
-        </button>
-      ) : undefined}{" "}
-      {props.score > 0 && props.score >= 10 ? (
-        <button
-          // onClick={() => {
-          //   setScore(0);
-          //   setMessage(message);
-          // }}
-          onClick={() => props.resetScore(props.id)}
-        >
-          Reset
-        </button>
-      ) : undefined}{" "}
-      <button
-        // onClick={() => {
-        //   setScore(score + 1);
-        //   setMessage(message);
-        // }}
-        onClick={() => props.incrementScoreByOne(props.id)}
-      >
-        + 1
-      </button>{" "}
-      <button
-        // onClick={() => {
-        //   setScore(score + 5);
-        //   setMessage(message);
-        // }}
-        onClick={() => props.incrementByFive(props.id)}
-      >
-        + 5
-      </button>{" "}
-      {props.score >= 10 && props.score < 80 ? (
-        <button
-          // onClick={() => {
-          //   setScore(score + score / 2);
-          //   setMessage(message);
-          // }}
-          onClick={() => props.incrementFifty(props.id)}
-        >
-          + 50%
-        </button>
-      ) : undefined}{" "}
-      <p>{displayMessage}</p>
+          + 5
+        </button>{" "}
+        {props.score >= 10 && props.score < 80 ? (
+          <button
+            // onClick={() => {
+            //   setScore(score + score / 2);
+            //   setMessage(message);
+            // }}
+            onClick={() => props.incrementFifty(props.id)}
+          >
+            + 50%
+          </button>
+        ) : undefined}{" "}
+        <p>{displayMessage}</p>
+      </div>
     </div>
   );
 }
